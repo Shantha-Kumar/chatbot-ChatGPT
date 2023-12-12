683ae39a7e994bfcc8fb73a535bf5994a0d4b0f5
Shantha Kumar <mailtoshan265@gmail.com>
2023-12-12 18:46:12 +0530

    Created chatbot

--- backend.py
+++ backend.py
@@ -1,2 +1,21 @@
+import openai
+
+
 class ChatBot:
-    pass
+    def __init__(self):
+        openai.api_key = "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"
+
+    def get_response(self, user_input):
+        response = openai.completions.create(
+            model='text-davinci-003',
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+
+if __name__ == "__main__":
+    chatbot = ChatBot()
+    response = chatbot.get_response("Where to report chatgpt bugs?")
+    print(response)
